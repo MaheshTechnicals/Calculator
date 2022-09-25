55e7e3e8c05896a2533eee73047d2f5d2df1a4fb
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-09-25 11:22:15 +0530

    Now You Can Calculate Numbers

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+// Get Numbers 
 
+function getNum(num) {
+  const input = document.querySelector("input")
+  input.value += num
+}
+
+
+// calculate Numbers.
+
+function result() {
+  const input = document.querySelector("input")
+  input.value = eval(input.value)
+}
